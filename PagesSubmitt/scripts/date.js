1d5c8837bd32a0f321910ff9e36184da7382919d
DBONOSO <david.bonoso21@gmail.com>
2023-02-04 01:03:13 -0300

    add

--- PagesSubmitt/scripts/date.js
+++ PagesSubmitt/scripts/date.js
@@ -28,5 +28,9 @@ let dayName = daynames[d.getDay()];
 let monthName = months[d.getMonth()];
 let year = d.getFullYear();
 let fulldate = dayName + ", " + monthName + " " + d.getDate() + " " + year;
-document.getElementById("date2").textContent = fulldate;
+document.getElementById("date2").textContent = year;
+document.getElementById("date3").textContent = fulldate;
+
+
+
 
