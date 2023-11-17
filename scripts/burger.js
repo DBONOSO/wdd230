8d62038a34490f9b185a1475451077b060e2543c
DBONOSO <david.bonoso21@gmail.com>
2023-11-17 02:35:46 -0300

    add

--- scripts/burger.js
+++ scripts/burger.js
@@ -4,3 +4,29 @@ function toggleMenu(){
 }
 const x = document.getElementById('hamburgerBtn');
 x.onclick=toggleMenu;
+
+
+//adding the account 
+
+function updateVisitCount() {
+  let visitCount = parseInt(document.getElementById('visit-count').textContent);
+  visitCount++;
+  document.getElementById('visit-count').textContent = visitCount;
+}
+
+window.onload = function() {
+  updateVisitCount();
+};
+
+function storeVisitCount() {
+  let visitCount = parseInt(document.getElementById('visit-count').textContent);
+  localStorage.setItem('visitCount', visitCount);
+}
+
+window.onload = function() {
+  updateVisitCount();
+  let storedVisitCount = localStorage.getItem('visitCount');
+  if (storedVisitCount) {
+    document.getElementById('visit-count').textContent = storedVisitCount;
+  }
+};
