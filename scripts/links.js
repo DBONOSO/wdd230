ef0030d77f0deb9296eb594824f1b2c23b878208
DBONOSO <david.bonoso21@gmail.com>
2023-12-05 21:39:27 -0300

    add

---
+++ scripts/links.js
@@ -0,0 +1,22 @@
+const baseURL = "https://dbonoso.github.io/wdd230/";
+const linksURL = "https://dbonoso.github.io/wdd230/data/links.json";
+const ul = document.querySelector(".homework");
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data.weeks);
+}
+
+getLinks();
+function displayLinks(weeks) {
+    weeks.forEach(weekindex => {
+        const li = document.createElement("li");
+        li.innerHTML = `${weekindex.week}:`
+        weekindex.links.forEach(link => {
+            li.innerHTML += ` <a href="${link.url}">${link.title}</a> |`
+        })
+        li.innerHTML = li.innerHTML.slice(0, -1);
+        ul.appendChild(li);
+    });
+}
