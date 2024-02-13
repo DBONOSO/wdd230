2d7ba8fef123e8a94be98eeaff36392e72b2b22b
DBONOSO <david.bonoso21@gmail.com>
2024-02-13 19:44:18 -0300

    addd

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,14 +1,40 @@
-function windChill(tempF, speed) {
-   if (tempF<=50 && speed >=3) {
-      var windChill = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, 0.16));
-      return windChill.toFixed(2) + "F";
-   } else {
-    return "It doesn't work"
-   }
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#caption');
+const windSpeedElement = document.querySelector('#wind-speed');
+
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=-31.53546&lon=-68.53&units=imperial&appid=38b07c8612ea622b365c841f50a776a2";
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-var temperature = parseFloat(prompt("Enter the air temperature in Fahrenheits:"));
-var windSpeed = parseFloat(prompt("Enter the wind speed in miles per hour;"));
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
 
-document.getElementById("chilling").innerHTML = windChill (temperature,windSpeed);
-document.getElementById("speeding").innerHTML = temperature;
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', `png of ${desc}`);
+    captionDesc.textContent = `${desc.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}`;
+    windSpeedElement.textContent = `Wind Speed: ${data.wind.speed} mph`;
+    }
